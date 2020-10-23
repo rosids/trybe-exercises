7f3d0110ed9a1f33f4b581b2315ca4f08b49fbe9
Rosiele David <rosieleserpa@hotmail.com>
2020-10-23 18:28:54 -0300

    feat: add exercise5.js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
@@ -0,0 +1,10 @@
+let angulo1 = 45
+let angulo2 = 44
+let angulo3 = 90
+
+if ((angulo1 + angulo2 + angulo3) == 180) {
+  console.log(true)
+} else {
+  console.log("false")
+  console.log("Não é um triângulo")
+}

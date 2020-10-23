b7337e4d6dfe1164a75e4f56d95beb8aef7a4959
Rosiele David <rosieleserpa@hotmail.com>
2020-10-23 18:58:46 -0300

    feat: correction exercise5.js

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise5.js
@@ -1,10 +1,14 @@
 let angulo1 = 45
-let angulo2 = 44
+let angulo2 = 45
 let angulo3 = 90
 
-if ((angulo1 + angulo2 + angulo3) == 180) {
+let soma = angulo1 + angulo2 + angulo3
+
+if (typeof soma != "number") {
+  console.log("Valor inválido")
+}
+else if (soma == 180) {
   console.log(true)
 } else {
   console.log("false")
-  console.log("Não é um triângulo")
 }

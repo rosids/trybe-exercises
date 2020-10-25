652e79b4c5b7433a2e71590e20f57c20b258e95a
Rosiele David <rosieleserpa@hotmail.com>
2020-10-25 15:24:51 -0300

    moved to another folder

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise2.js
+++
@@ -1,8 +0,0 @@
-let a = 10;
-let b = 15;
-
-if(a > b) {
-  console.log(a);
-} else {
-  console.log(b);
-}
--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercise4.js
+++
@@ -1,9 +0,0 @@
-let valor = 10
-
-if (valor > 0) {
-  console.log("positive")
-} else if (valor < 0) {
-  console.log("negative")
-} else {
-  console.log("zero")
-}

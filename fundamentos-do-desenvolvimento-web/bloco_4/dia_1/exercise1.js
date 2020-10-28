732fee8ba5a1f0a7014f669b2cb19b5d74f5e80f
Rosiele David <rosieleserpa@hotmail.com>
2020-10-28 10:49:11 -0300

    build: rearranging files in folders

---
+++ fundamentos-do-desenvolvimento-web/bloco_4/dia_1/exercise1.js
@@ -0,0 +1,14 @@
+let a = 10;
+let b = 5;
+
+let soma = a + b;
+let subtracao = a - b;
+let multiplacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(soma)
+console.log(subtracao)
+console.log(multiplacao)
+console.log(divisao)
+console.log(modulo)

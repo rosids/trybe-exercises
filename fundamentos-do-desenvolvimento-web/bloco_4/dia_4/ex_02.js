1feec28dff2f59da411d30afd64d861e72e68ed5
Rosiele David <rosieleserpa@hotmail.com>
2020-10-29 21:45:32 -0300

    build: files in the wrong branch

--- fundamentos-do-desenvolvimento-web/bloco_4/dia_4/ex_02.js
+++
@@ -1,9 +0,0 @@
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-};
-
-info['recorrente'] = "Sim";
-
-console.log(info);

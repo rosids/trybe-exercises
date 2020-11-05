5cbb3ddbf98cfbf05394e105d401c86238783f20
Rosiele David <rosieleserpa@hotmail.com>
2020-11-05 20:07:08 -0300

    feat: add exercise 1

--- fundamentos-do-desenvolvimento-web/bloco_5/dia_3/script.js
+++ fundamentos-do-desenvolvimento-web/bloco_5/dia_3/script.js
@@ -13,4 +13,29 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
-// Escreva seu código abaixo.
+// Exercise 1
+function createDates(){
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const days = document.getElementById('days');
+  
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const date = dezDaysList[index];
+    const itemsList = document.createElement('li');
+    itemsList.className = 'day';
+    if(date === 24 || date === 31) {
+      itemsList.className = 'day holiday';
+      itemsList.innerHTML = date;
+    } else if (date === 4 || date === 11 || date === 18) {
+      itemsList.className = 'day friday';
+      itemsList.innerHTML = date;
+    } else if (date === 25) {
+      itemsList.className = 'day holiday friday';
+      itemsList.innerHTML = date;
+    }
+    itemsList.innerHTML = date;
+    days.appendChild(itemsList);
+  }
+  
+}
+
+createDates();

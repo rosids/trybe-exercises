1f714b80ce18f0869a2f8e030780a46af1583fd9
Rosiele David <rosieleserpa@hotmail.com>
2020-11-12 23:21:38 -0300

    feat: add function to insert states

---
+++ fundamentos-do-desenvolvimento-web/bloco_6/dia_1/script.js
@@ -0,0 +1,42 @@
+const states = {
+  'AC': 'Acre',
+  'AL': 'Alagoas',
+  'AP': 'Amapá',
+  'AM': 'Amazonas',
+  'BA': 'Bahia',
+  'CE': 'Ceará',
+  'DF': 'Distrito Federal',
+  'ES': 'Espírito Santo',
+  'GO': 'Goiás',
+  'MA': 'Maranhão',
+  'MT': 'Mato Grosso',
+  'MS': 'Mato Grosso do Sul',
+  'MG': 'Minas Gerais',
+  'PA': 'Pará',
+  'PB': 'Paraíba',
+  'PR': 'Paraná',
+  'PE': 'Pernambuco',
+  'PI': 'Piauí',
+  'RJ': 'Rio de Janeiro',
+  'RN': 'Rio Grande do Norte',
+  'RS': 'Rio Grande do Sul',
+  'RO': 'Rondônia',
+  'RR': 'Roraima',
+  'SC': 'Santa Catarina',
+  'SP': 'São Paulo',
+  'SE': 'Sergipe',
+  'TO': 'Tocantins'
+};
+
+const stateList = document.querySelector('#state');
+
+function insertStates() {
+  for (let state in states) {
+    const itemState = document.createElement('option');
+    itemState.value = state;
+    itemState.innerHTML = states[state];
+    stateList.appendChild(itemState);
+  }
+}
+
+insertStates();

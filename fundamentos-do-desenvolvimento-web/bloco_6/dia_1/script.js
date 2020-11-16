645cfc6947e2853a578e3803de1304247ba414b0
Rosiele David <rosieleserpa@hotmail.com>
2020-11-15 23:48:03 -0300

    feat: add exercise 2

--- fundamentos-do-desenvolvimento-web/bloco_6/dia_1/script.js
+++ fundamentos-do-desenvolvimento-web/bloco_6/dia_1/script.js
@@ -40,3 +40,62 @@ function insertStates() {
 }
 
 insertStates();
+
+function dateValidation() {
+  const inputDate = document.querySelector('#date');
+  const date = inputDate.value;
+  const arrayDate = date.split('/');
+  const day = parseInt(arrayDate[0]);
+  const month = parseInt(arrayDate[1]);
+  const year = parseInt(arrayDate[2]);
+
+  if (!(day > 0 && day <= 31 && month > 0 && month <= 12 && year > 0)) {
+    inputDate.classList.add('error');
+    return 'Erro na validação da data \n';
+  }
+  return '';
+
+}
+
+const inputText = document.querySelectorAll('input[type=text], input[type=email], select, textarea');
+
+function fieldValidation() {
+  let erro = dateValidation();
+  let erros = '';
+
+  if (erro != '') {
+    erros += `${erro} \n`;
+  }
+
+  for(let index = 0; index < inputText.length; index += 1) {
+    const input = inputText[index];
+    if(input.required) {
+      if(input.value === '') {
+        input.classList.add('error');
+        erros += `O campo ${input.name} é obrigatório \n`;
+      }
+      if (input.minlength && input.minlength.length < input.minlength) {
+        input.classList.add('error');
+        erros += `O campo ${input.id} deve possuir no mínimo ${input.length} caracteres \n`;
+      }
+    }
+  }
+
+  if(erros != '') {
+    alert(erros);
+  } else {
+    const form = document.querySelector('form')
+    form.reset();
+    for(let index = 0; index < inputText.length; index += 1) {
+      inputText[index].classList.remove('error');
+    }
+  }
+}
+
+const buttonSubmit = document.querySelector('#submit');
+
+buttonSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  fieldValidation();
+  
+});

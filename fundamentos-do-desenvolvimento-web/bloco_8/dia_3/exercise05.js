70a2cf8e68cadad430eb53a58ab56de8c18af6a8
Rosiele David <rosieleserpa@hotmail.com>
2020-12-02 14:57:27 -0300

    feat: Initial files for the exercises

---
+++ fundamentos-do-desenvolvimento-web/bloco_8/dia_3/exercise05.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+const names = [
+  'Aanemarie',  'Adervandes',   'Akifusa',
+  'Abegildo',   'Adicellia',    'Aladonata',
+  'Abeladerco', 'Adieidy',  'Alarucha',
+];
+
+
+
+function containsA() {
+  // escreva seu código aqui
+}
+
+assert.deepEqual(containsA(), 20);

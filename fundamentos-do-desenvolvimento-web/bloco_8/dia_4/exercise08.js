01089f38b4622a7122f211f78032fb49a6af57ea
Rosiele David <rosieleserpa@hotmail.com>
2020-12-03 19:32:03 -0300

    feat: add exercise 08

--- fundamentos-do-desenvolvimento-web/bloco_8/dia_4/exercise08.js
+++ fundamentos-do-desenvolvimento-web/bloco_8/dia_4/exercise08.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-// escreva greet abaixo
+const greet = (name, greeting = 'Hi') => `${greeting} ${name}`;
 
 assert.strictEqual(greet("John"), "Hi John")
 assert.strictEqual(greet("John", "Good morning"), "Good morning John");

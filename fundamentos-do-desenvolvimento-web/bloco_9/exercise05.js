15cc947c87170d49a68565d0bf69c2b026fa352c
Rosiele David <rosieleserpa@hotmail.com>
2020-12-08 19:21:05 -0300

    feat: add exercise 05

--- fundamentos-do-desenvolvimento-web/bloco_9/exercise05.js
+++ fundamentos-do-desenvolvimento-web/bloco_9/exercise05.js
@@ -10,7 +10,12 @@ const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${
 const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
-
+const sendMarsTemperature = callback => {
+  const temperature = getMarsTemperature();
+  setTimeout(() => {
+    callback(temperature);
+  }, messageDelay());
+};
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo

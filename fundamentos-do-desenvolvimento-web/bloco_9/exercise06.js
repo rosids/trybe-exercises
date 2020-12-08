0b7aa3afd223e6aa6be7d4c6df3c9d9a99678563
Rosiele David <rosieleserpa@hotmail.com>
2020-12-08 19:35:03 -0300

    feat: add exercise 06

--- fundamentos-do-desenvolvimento-web/bloco_9/exercise06.js
+++ fundamentos-do-desenvolvimento-web/bloco_9/exercise06.js
@@ -12,7 +12,14 @@ const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now 
 const handleError = (errorReason) => console.log(`Error getting temperature: ${errorReason}`);
 
 // definição da função sendMarsTemperature...
-
+const sendMarsTemperature = (sucess, error) => {
+  const temperature = getMarsTemperature();
+  const operationSucceed = Math.random() >= 0.5;
+  setTimeout(() => {
+    if(operationSucceed) sucess(temperature);
+    else error('Robot is busy');
+  }, messageDelay());
+};
 
 // imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
 sendMarsTemperature(temperatureInFahrenheit, handleError);

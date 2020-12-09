de8f964c7211e970bba975f776586c9dfaf981ef
Rosiele David <rosieleserpa@hotmail.com>
2020-12-09 15:33:02 -0300

    feat: add exercise 01

--- fundamentos-do-desenvolvimento-web/bloco_9/dia_2/script.js
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_2/script.js
@@ -1,7 +1,16 @@
 const API_URL = 'https://icanhazdadjoke.com/';
+const myObject = {
+  method: 'GET',
+  headers: { 'Accept': 'application/json' }
+};
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => {
+    const jokeContainer = document.getElementById('jokeContainer');
+    jokeContainer.innerHTML = data.joke;
+  });
 };
 
 window.onload = () => fetchJoke();

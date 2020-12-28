15b0869a8bf7464fa81bc4d94881063f2b10fbd1
Rosiele David <rosieleserpa@hotmail.com>
2020-12-28 18:33:34 -0300

    feat: refactor ex02

--- fundamentos-do-desenvolvimento-web/bloco_9/dia_2/ex02.js
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_2/ex02.js
@@ -1,11 +1,11 @@
+const randomNumbers = () => Array.from({ length: 10 }, () =>  Math.floor(Math.random() * 50) + 1);
+const sumNumbers = array => array.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+
 const fetchPromise = () => {
   const myPromise = new Promise((resolve, reject) => {
-    const array = Array.from(
-      { length: 10 },
-      () =>  Math.floor(Math.random() * 50) + 1
-    );
+    const array = randomNumbers();
 
-    const sum = array.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+    const sum = sumNumbers(array);
     console.log(sum);
 
     (sum < 8000) ? resolve() : reject();

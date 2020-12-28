3f7f657f1edc781c1ad16a65f6343ee044f1ba96
Rosiele David <rosieleserpa@hotmail.com>
2020-12-28 18:21:44 -0300

    feat: refactor ex04

--- fundamentos-do-desenvolvimento-web/bloco_9/dia_2/ex04.js
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_2/ex04.js
@@ -1,18 +1,21 @@
+const randomNumbers = () => Array.from({ length: 10 }, () =>  Math.floor(Math.random() * 50) + 1);
+const sumNumbers = array => array.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+
+const createArrayWithDivisionNumbers = (div, array) => array.map(number => div / number);
+
 const fetchPromise = () => {
   const myPromise = new Promise((resolve, reject) => {
-    const array = Array.from(
-      { length: 10 },
-      () =>  Math.floor(Math.random() * 50) + 1
-    );
+    const array = randomNumbers();
 
-    const sum = array.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+    const sum = sumNumbers(array);
     console.log(sum);
 
     (sum < 8000) ? resolve(sum) : reject();
   });
 
   myPromise
-  .then(div => console.log([2, 3, 5, 10].map(number => div / number)))
+  .then(div => console.log(createArrayWithDivisionNumbers(div, [2, 3, 5, 10])))
+
   .catch(() => console.log('É mais de oito mil! Essa promise deve estar quebrada!'));
 };
 

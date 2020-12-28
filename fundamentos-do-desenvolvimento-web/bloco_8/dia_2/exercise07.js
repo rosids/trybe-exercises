d620f82a56657d164c09a3c9cc27484516a9972b
Rosiele David <rosieleserpa@hotmail.com>
2020-12-28 15:02:41 -0300

    feat: add exercise 07

--- fundamentos-do-desenvolvimento-web/bloco_8/dia_2/exercise07.js
+++ fundamentos-do-desenvolvimento-web/bloco_8/dia_2/exercise07.js
@@ -66,7 +66,9 @@ const books = [
 const expected_result = 'O Senhor dos Anéis';
 
 function authorWith3DotsOnName() {
-  // escreva seu código aqui
+  return books.find(book => (
+    book.author.name.split(' ').filter(word => word.endsWith('.')).length === 3
+  )).name;
 }
 
 assert.deepEqual(authorWith3DotsOnName(), expected_result);

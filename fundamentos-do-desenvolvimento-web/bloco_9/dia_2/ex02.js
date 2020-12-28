0c41ce799d3143697e470d8c80df90a3af1dcf99
Rosiele David <rosieleserpa@hotmail.com>
2020-12-28 17:31:01 -0300

    feat: add exercise 02

---
+++ fundamentos-do-desenvolvimento-web/bloco_9/dia_2/ex02.js
@@ -0,0 +1,19 @@
+const fetchPromise = () => {
+  const myPromise = new Promise((resolve, reject) => {
+    const array = Array.from(
+      { length: 10 },
+      () =>  Math.floor(Math.random() * 50) + 1
+    );
+
+    const sum = array.map(number => number * number).reduce((acc, cur) => acc + cur, 0);
+    console.log(sum);
+
+    (sum < 8000) ? resolve() : reject();
+  });
+
+  myPromise
+  .then(() => console.log('Deu bom!'))
+  .catch(() => console.log('Deu ruim!'));
+};
+
+fetchPromise();

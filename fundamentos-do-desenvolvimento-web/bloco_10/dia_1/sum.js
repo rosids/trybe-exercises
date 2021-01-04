598d875d748806b245dfbe36551b523806e46caa
Rosiele David <rosieleserpa@hotmail.com>
2021-01-04 16:30:10 -0300

    feat: add exercise part 1

---
+++ fundamentos-do-desenvolvimento-web/bloco_10/dia_1/sum.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+module.exports = sum;

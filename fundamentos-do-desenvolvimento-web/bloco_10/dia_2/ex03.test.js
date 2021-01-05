43929bc7c07d829afaf3aa88a6b254d32f3f8b6a
Rosiele David <rosieleserpa@hotmail.com>
2021-01-05 20:07:28 -0300

    feat: add exercise 3 from block 10.2

--- fundamentos-do-desenvolvimento-web/bloco_10/dia_2/ex03.test.js
+++ fundamentos-do-desenvolvimento-web/bloco_10/dia_2/ex03.test.js
@@ -1,1 +1,21 @@
 const getUserName = require('./users');
+
+describe('test async/await - getUserName', () => {
+  describe('When the user ID exists', () => {
+    it('must return the user name', async () => {
+      expect.assertions(1);
+      await expect(getUserName(4)).resolves.toEqual('Mark');
+    });
+  });
+
+  describe(`When the user ID doesn't exist`, () => {
+    it('returns an error', async () => {
+      expect.assertions(1);
+      const id = 3;
+      await expect(getUserName(id)).rejects.toEqual({ error: `User with ${id} not found.` })
+    });
+  });
+});     
+
+//   describe(`When the user ID doesn't exist`, () => {
+//     it('returns an error', 

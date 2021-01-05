c850d27f7b2acb92e900462d9f9c9f8cf6600133
Rosiele David <rosieleserpa@hotmail.com>
2021-01-05 18:55:00 -0300

    feat: add exercise 1 from block 10.2

--- fundamentos-do-desenvolvimento-web/bloco_10/dia_2/ex01.test.js
+++ fundamentos-do-desenvolvimento-web/bloco_10/dia_2/ex01.test.js
@@ -1,3 +1,10 @@
 const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
+
+test('checks the callback of the UpperCase function', (done) => {
+  uppercase('Hello', (callback) => {
+    expect(callback).toBe('HELLO');
+    done();
+  });
+});

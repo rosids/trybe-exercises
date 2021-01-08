644665c69c4ece66aae7a93e1df3df3a7bc1db38
Rosiele David <rosieleserpa@hotmail.com>
2021-01-08 12:16:11 -0300

    feat: add exercise 7 from block 10.3

--- fundamentos-do-desenvolvimento-web/bloco_10/dia_3/ex07.test.js
+++ fundamentos-do-desenvolvimento-web/bloco_10/dia_3/ex07.test.js
@@ -5,3 +5,18 @@ const fetchJoke = () => {
     .then(response => response.json())
     .then(data => data.joke);
 };
+
+const fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve({
+        id: "7h3oGtrOfxc",
+        joke: "Whiteboards ... are remarkable.",
+        status: 200
+      })
+  })
+);
+
+test("should check the API call for a specific result", () => {
+  expect(fetchJoke()).resolves.toBe('Whiteboards ... are remarkable.');
+});

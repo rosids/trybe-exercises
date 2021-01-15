1c1c8f020dd885500f0b350e0a1dfe48178f1340
unknown <rosieleserpa@hotmail.com>
2021-01-15 00:44:53 -0300

    feat: add exercise 1 from block 11.2

--- desenvolvimento-front-end/bloco_11/dia_02/my-pokedex/src/Pokemon.js
+++ desenvolvimento-front-end/bloco_11/dia_02/my-pokedex/src/Pokemon.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import './Pokemon.css';
+
 class Pokemon extends Component {
   render() {
+    const { name, type, averageWeight, image } = this.props.pokemon;
+
     return (
-      <div>
-        
-      </div>
+      <section className='Pokemon'>
+        <p> {name} </p>
+        <p> {type} </p>
+        <p>{`Average weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        <img src={image} alt={`${name} sprite`} />
+      </section>
     );
   }
 }
 
 Pokemon.propTypes = {
-
+  pokemon: PropTypes.shape({
+    name: PropTypes.string,
+    type: PropTypes.string,
+    averageWeight: PropTypes.shape({
+      measurementUnit: PropTypes.string,
+      value: PropTypes.number
+    }),
+    image: PropTypes.string,
+  }).isRequired,
 };
 
 export default Pokemon;

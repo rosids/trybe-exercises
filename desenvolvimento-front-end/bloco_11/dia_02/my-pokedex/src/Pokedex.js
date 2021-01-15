6841c142d557b0cf908348b25e39956d45b06546
Rosiele David <seu-email@example.com>
2021-01-15 00:11:58 -0300

    build: add the initial files for exercise 11.1

---
+++ desenvolvimento-front-end/bloco_11/dia_02/my-pokedex/src/Pokedex.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Pokedex extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+Pokedex.propTypes = {
+
+};
+
+export default Pokedex;

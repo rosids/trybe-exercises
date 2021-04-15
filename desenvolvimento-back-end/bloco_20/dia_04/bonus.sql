f2c0300f33662b18729e11cadcd6f2bdaa7a9ab0
Rosiele David <rosieleserpa@hotmail.com>
2021-04-15 11:32:31 -0300

    feat: add bonus exercise resolutions from block 20.4

---
+++ desenvolvimento-back-end/bloco_20/dia_04/bonus.sql
@@ -0,0 +1,7 @@
+#Exercício 8
+SELECT * FROM Pixar.BoxOffice WHERE domestic_sales > 400000000;
+UPDATE Pixar.BoxOffice SET rating = 9.0 WHERE movie_id IN (6, 10);
+
+SELECT * FROM Pixar.Movies;
+SELECT * FROM Pixar.BoxOffice;
+#Exercício 9

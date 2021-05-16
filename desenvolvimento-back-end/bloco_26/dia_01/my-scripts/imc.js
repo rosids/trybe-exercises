56d1f20c88828bced7c8628c5968193cf87a138a
Rosiele David <rosieleserpa@hotmail.com>
2021-05-16 18:15:33 -0300

    feat: Add exercises 3 and 4 from block 26.1

--- desenvolvimento-back-end/bloco_26/dia_01/my-scripts/imc.js
+++ desenvolvimento-back-end/bloco_26/dia_01/my-scripts/imc.js
@@ -1,9 +1,11 @@
+const readline = require("readline-sync");
+
 function calculaImc() {
-  const peso = 70;
-  const altura = 1.69;
+  const peso = readline.questionFloat("Qual seu peso? (em kg) ");
+  const altura = readline.questionFloat("Qual sua altura? (em metros) ");
 
   const imc = (peso / altura ** 2).toFixed(2);
-  
+
   console.log(`Seu IMC é ${imc}`);
 }
 

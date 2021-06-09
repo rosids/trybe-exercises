eb70a2314ee63bbdaf5a4e6e588cd32d47851afd
Rosiele David <rosieleserpa@hotmail.com>
2021-06-09 20:17:03 -0300

    feat: Add exercise 2 from block 28.2

--- desenvolvimento-back-end/bloco_28/multer-exercises/index.js
+++ desenvolvimento-back-end/bloco_28/multer-exercises/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const { resolve } = require('path');
+const fs = require('fs');
 
 const { PORT = 3000 } = process.env;
 
@@ -28,7 +29,28 @@ const storage = multer.diskStorage({
   filename: (_req, file, callback) => callback(null, `${Date.now()}-${file.originalname}`),
 });
 
-const upload = multer({ storage });
+const fileExists = (fileName) => {
+  const files = fs.readdirSync(resolve(__dirname, 'uploads'));
+
+  return files.some((file) => file.includes(fileName));
+};
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype !== 'image/png') {
+    req.fileValidationError = { error: { statusCode: 403, message: 'Extension must be `png`' } };
+
+    return cb(null, false);
+  }
+
+  if (fileExists(file.originalname)) {
+    req.fileDuplicated = { error: { statusCode: 409, message: 'File already exists' } };
+    return cb(null, false);
+  }
+
+  cb(null, true);
+};
+
+const upload = multer({ storage, fileFilter });
 
 app.get('/ping', controllers.ping);
 

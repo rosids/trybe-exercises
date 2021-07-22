f0df47ad8bd302d8b66b772686048611e8ebaffb
Rosiele David <rosieleserpa@hotmail.com>
2021-07-22 19:18:57 -0300

    feat: add exercise 17 from block 24.3

--- desenvolvimento-back-end/bloco_24/dia_03/exercises.js
+++ desenvolvimento-back-end/bloco_24/dia_03/exercises.js
@@ -154,3 +154,13 @@ db.movies.updateOne(
     },
   },
 );
+
+// Exercício 17
+db.movies.updateOne(
+  { title: 'Home Alone' },
+  {
+    $set: {
+      description: 'An eight-year-old troublemaker must protect his house from a pair of burglars when he is accidentally left home alone by his family during Christmas vacation.',
+    },
+  },
+);

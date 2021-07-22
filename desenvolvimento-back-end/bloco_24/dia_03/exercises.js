1413bef4775dc26dd4e78e2c3d16fb50c9297748
Rosiele David <rosieleserpa@hotmail.com>
2021-07-22 18:24:48 -0300

    feat: add exercise 4 from block 24.3

--- desenvolvimento-back-end/bloco_24/dia_03/exercises.js
+++ desenvolvimento-back-end/bloco_24/dia_03/exercises.js
@@ -24,3 +24,16 @@ db.movies.updateOne(
     },
   },
 );
+
+// Exercício 4
+db.movies.updateOne(
+  { title: 'Godzilla' },
+  {
+    $push: {
+      ratings: {
+        $each: [78, 52, 95, 102],
+      },
+    },
+  },
+  { upsert: true },
+);

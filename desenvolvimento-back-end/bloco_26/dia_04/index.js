d1bb0d7e751e374dbd3af5ca0d581329002e4eae
Rosiele David <rosieleserpa@hotmail.com>
2021-08-07 14:15:31 -0300

    feat: add exercise 1 from block 26.4

--- desenvolvimento-back-end/bloco_26/dia_04/index.js
+++ desenvolvimento-back-end/bloco_26/dia_04/index.js
@@ -5,5 +5,7 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+
 const { PORT = 3000 } = process.env;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));

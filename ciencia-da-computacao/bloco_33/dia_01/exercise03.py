56dbd39d06eb21dab198306731bef8cb55c265a2
Rosiele David <rosieleserpa@hotmail.com>
2021-08-13 19:54:58 -0300

    feat: add exercise 3 from block 33.1

---
+++ ciencia-da-computacao/bloco_33/dia_01/exercise03.py
@@ -0,0 +1,8 @@
+def perfect_square_asterisks(number):
+    """
+    Given any value n, such that n> 1, print a square made up of side
+    asterisks of size n on the screen.
+    """
+    for row in range(number):
+        print(number * "*")
+    return ""
